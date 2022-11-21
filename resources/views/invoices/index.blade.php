eee457ae8f73dd59c9cfd8f42ce2619b8f3516fc
Tomasz D <gorky23@o2.pl>
2022-11-21 09:00:19 +0100

    Add bootstrap style

--- resources/views/invoices/index.blade.php
+++ resources/views/invoices/index.blade.php
@@ -2,13 +2,13 @@
 @section('content')
 
     @if(session()->has('message'))
-    <div class="alert alert-success alert-dismissible fade show col-6" role="alert">
+    <div class="alert alert-success alert-dismissible fade show col-2" role="alert">
         {{ session()->get('message') }}
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
     @endif
 
-    <div class="container d-flex justify-content-center pt-5 col-8">
+    <div class="container d-flex justify-content-center col-8">
         <div class="col-12 mt-5" style="border-radius: 5px; background-color: #f2f2f2; padding: 40px; opacity: 0.8;">
           <h1 style="padding-bottom: 10px;">Lista faktur</h1>
           <div class="d-flex justify-content-center align-items-center mx-4">
@@ -29,12 +29,12 @@
                         <td>{{ $invoice->number }}</td>
                         <td>{{ $invoice->date }}</td>
                         <td>{{ $invoice->total }}</td>
-                        <td><a href="{{ route('invoices.edit', ['id' => $invoice->id]) }}" class="btn btn-primary">Edytuj</a>
+                        <td><div class="btn-group"><a href="{{ route('invoices.edit', ['id' => $invoice->id]) }}" class="btn btn-primary btn-sm" style="margin-right: 3px;">Edytuj</a>
                             <form method="POST" action="{{ route('invoices.delete', ['id' => $invoice->id]) }}">
                                 @csrf
                                 @method('DELETE')
-                                <button type="submit" class="btn btn-danger">Usuń</button>
-                            </form>
+                                <button type="submit" class="btn btn-danger btn-sm">Usuń</button>
+                            </form></div>
                         </td>
                       </tr>
                     @endforeach
